4a31e37b037b8f2e495934b4d4a3b31767430af8
bergermann <dominik.bergermann@gmx.de>
2023-06-05 12:13:55 +0200

    init

--- src/neldermead/simplexinit.jl
+++ src/neldermead/simplexinit.jl
@@ -3,8 +3,8 @@
 export initSimplexCoord, initSimplexCoord!, initSimplexAffine
 export InitSimplexCoord
 
-
-function initSimplexCoord(x0::Array{Float64},d::Real,args::Tuple{})
+# args = (d,)
+function initSimplexCoord(x0::Array{Float64},args::Tuple{})
     x = repeat(x0,1,length(x0)+1)
 
     for i in 1:length(x0)
@@ -16,7 +16,12 @@ end
 
 const InitSimplexCoord(d) = Callback(initSimplexCoord,(d,))
 
-function initSimplexAffine(x0::Array{Float64},p::Matrix{Float64},a::Float64,
-                                                    b::Float64,args::Tuple{})
+
+
+# args = (p::Matrix{Float64},a::Float64,b::Float64,)
+function initSimplexAffine(x0::Array{Float64},args::Tuple{})
+    x = repeat(x0,1,length(x0)+1)
     x[:,:]
+
+    return x
 end
