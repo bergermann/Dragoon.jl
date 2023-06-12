fa5352bf2002a16b176972aa26d500a6cef5b356
bergermann <dominik.bergermann@gmx.de>
2023-06-12 15:22:56 +0200

    init

--- src/optimizers.jl
+++ src/optimizers.jl
@@ -1,6 +1,6 @@
 ###     main file, optimization algorithms
 
-export linesearch, nelderMead
+export linesearch, nelderMead, simulatedAnnealing
 
 function linesearch(booster::Booster,hist::Vector{State},freqs::Array{Float64},
                     α::Float64,
@@ -275,164 +275,20 @@ function nelderMead(booster::Booster,hist::Vector{State},freqs::Array{Float64},
 end
 
 
-# function nelderMead(booster::PhysicalBooster,hist::Vector{State},freqs::Array{Float64},
-#         α::Float64,β::Float64,γ::Float64,δ::Float64,
-#         objFunction::Tuple{Function,Vector},
-#         initSimplex::Tuple{Function,Vector},
-#         simplexObj::Tuple{Function,Vector},
-#         unstuckinator::Tuple{Function,Vector};
-#         maxiter::Integer=Int(1e2),
-#         showtrace::Bool=false,
-#         showevery::Integer=1,
-#         unstuckisiter::Bool=true,
-#         tracecentroid::Bool=false,
-#         traceevery::Int=1)
 
-#     t = now(UTC)
-
-#     trace = Vector{NMTrace}(undef,Int(maxiter/traceevery)+1)
-
-#     x = zeros(booster.ndisk,booster.ndisk+1)
-#     f = zeros(booster.ndisk+1)
-
-#     initSimplex[1](booster.pos,x,initSimplex[2]...)
-#     f = simplexObj[1](x,Vector(1:booster.ndisk+1),booster,hist,freqs,
-#                                 objFunction,simplexObj[2]...)
-
-#     i = 0
-#     while i < maxiter
-#         i += 1
-
-#         #sort
-#         sp = sortperm(f; rev=false)
-#         x = x[:,sp]
-#         f = f[sp]
-
-#         if Int(i%traceevery)==0
-#             trace[Int(i/traceevery)] = NMTrace(x,f,zeros(booster.ndisk),0.,
-#                             booster.timestamp,booster.summedtraveltime)
-#         end
-
-#         #centroid
-#         x_ = reshape(sum(x[:,1:end-1]; dims=2),:)/booster.ndisk
-#         if tracecentroid
-#             move(booster,x_; additive=false)
-#             updateHist!(booster,hist,freqs,objFunction)
-
-#             trace[i].x_ = x_
-#             trace[i].obj = hist[1].objvalue
-#         end
-
-#         #reflection point, reflection
-#         xr = x_+α*(x_-x[:,end])
-#         move(booster,xr; additive=false)
-#         updateHist!(booster,hist,freqs,objFunction)
-#         fr = hist[1].objvalue
-
-#         if f[1] <= fr < f[end-1]
-#             x[:,end] = xr
-#             f[end] = fr
-#         elseif fr < f[1]    #expansion
-#             xe = x_+β*(xr-x_)
-
-#             move(booster,xe; additive=false)
-#             updateHist!(booster,hist,freqs,objFunction)
-
-#             fe = hist[1].objvalue
-
-#             if fe < fr
-#                 x[:,end] = xe
-#                 f[end] = fe
-#             else
-#                 x[:,end] = xr
-#                 f[end] = fr
-#             end
-#         else #if fr >= f[end-1] #contraction
-#             if f[end-1] <= fr < f[end] #outside contraction
-#                 xoc = x_+γ*(xr-x_)
-
-#                 move(booster,xoc; additive=false)
-#                 updateHist!(booster,hist,freqs,objFunction)
-
-#                 foc = hist[1].objvalue
-
-#                 if foc <= fr
-#                     x[:,end] = xoc
-#                     f[end] = foc
-#                 else #shrink
-#                     for j in 2:booster.ndisk+1
-#                         v = x[:,1]+δ*(x[:,j]-x[:,1])
-#                         x[:,j] = v
-#                     end
-#                     f[2:end] = simplexObj[1](x,Vector(2:booster.ndisk+1),
-#                                 booster,hist,freqs,objFunction,simplexObj[2]...)
-#                 end
-#             else #inside contraction
-#                 xic = x_-γ*(xr-x_)
-
-#                 move(booster,xic; additive=false)
-#                 updateHist!(booster,hist,freqs,objFunction)
-
-#                 fic = hist[1].objvalue
-
-#                 if fic < fr
-#                     x[:,end] = xic
-#                     f[end] = fic
-#                 else #shrink
-#                     for j in 2:booster.ndisk+1
-#                         v = x[:,1]+δ*(x[:,j]-x[:,1])
-#                         x[:,j] = v
-#                     end
-#                     f[2:end] = simplexObj[1](x,Vector(2:booster.ndisk+1),
-#                                 booster,hist,freqs,objFunction,simplexObj[2]...)
-#                 end
-#             end
-#         end
-
-#         showtrace && i%showevery==0 && printNMIter(booster,f,i)
-
-#         #iteration end
-#     end
-
-#     #sort and trace the end result
-#     sp = sortperm(f; rev=false)
-#     x = x[:,sp]
-#     f = f[sp]
-
-#     trace[Int(i/traceevery)+1] = NMTrace(x,f,zeros(booster.ndisk),0.,booster.timestamp,
-#                                     booster.summedtraveltime)
-
-#     x_ = reshape(sum(x[:,1:end-1]; dims=2),:)/booster.ndisk
-#     if tracecentroid
-#         move(booster,x_; additive=false)
-#         updateHist!(booster,hist,freqs,objFunction)
-
-#         trace[Int(i/traceevery)+1].x_ = x_
-#         trace[Int(i/traceevery)+1].obj = hist[1].objvalue
-#     end
-
-#     #move to optimal point
-#     move(booster,x[:,argmin(f)]; additive=false)
-#     updateHist!(booster,hist,freqs,objFunction)
-
-#     # booster.codetimestamp = canonicalize(now(UTC)-t)
-#     # printTermination(booster,hist,i,maxiter)
-
-#     return trace[1:Int(i/traceevery)+1]
-# end
 
 
 function simulatedAnnealing(booster::Booster,hist::Vector{State},freqs::Array{Float64},
-                        τ::Array{Float64},rmax::Float64,
-                        objFunction::Callback,
-                        unstuckinator::Callback;
-                        maxiter::Integer=Int(1e2),
-                        showtrace::Bool=false,
-                        showevery::Integer=1,
-                        unstuckisiter::Bool=true,
-                        traceevery::Int=1,
-                        resettimer::Bool=true)
-    
+        τ::Array{Float64},rmax::Float64,
+        objFunction::Callback,
+        unstuckinator::Callback;
+        maxiter::Integer=Int(1e2),
+        showtrace::Bool=false,
+        showevery::Integer=1,
+        unstuckisiter::Bool=true,
+        traceevery::Int=1,
+        resettimer::Bool=true)
+
     if hasproperty(booster,:startingtime) && resettimer
         booster.startingtime = unow()
     elseif hasproperty(booster,:codetimestamp)
@@ -440,17 +296,20 @@ function simulatedAnnealing(booster::Booster,hist::Vector{State},freqs::Array{Fl
     end
 
     trace = Vector{SATrace}(undef,maxiter+1)
-    
+
     updateHist!(booster,hist,freqs,objFunction)
 
     xsol = copy(booster.pos)
     objsol = hist[1].objvalue
-    
+
     x = copy(booster.pos)
     objx = hist[1].objvalue
 
 
-    for i in eachindex(τ)
+    iter = 1
+    i = 1
+    n_τ = length(τ)
+    while iter <= maxiter && i <= n_τ
         move(booster,x+findNeighbour(booster,rmax); additive=false)
         updateHist!(booster,hist,freqs,objFunction)
 
@@ -463,7 +322,12 @@ function simulatedAnnealing(booster::Booster,hist::Vector{State},freqs::Array{Fl
             xsol = copy(booster.pos)
         end
 
-        trace[i] = SATrace(x,obj,τ,iter,t,T)
+        trace[iter] = SATrace(x,obj,τ[i],iter,booster.timestamp,booster.summedtraveltime)
+
+        ## unstucking, alter i
+
+        iter += 1
+        i += 1
     end
 
 
@@ -472,7 +336,7 @@ function simulatedAnnealing(booster::Booster,hist::Vector{State},freqs::Array{Fl
 
 
 
-    
+
     move(booster,xsol; additive=false)
     updateHist!(booster,hist,freqs,objFunction)
 
