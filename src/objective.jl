cf5e524270278eb22c9c430c12600c36ef82bc24
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-06-27 17:29:11 +0200

    ref obj

---
+++ src/objective.jl
@@ -0,0 +1,29 @@
+###     how to calculate boost and objective functions
+
+export getObjAna1d
+export ObjAnalytical
+
+function getObjAna1d(booster::Booster,freqs::Array{Float64},args::Tuple{})
+    return -minimum(boost1d(pos2dist(booster.pos; thickness=booster.thickness),
+        freqs; eps=booster.epsilon,thickness=booster.thickness))
+end
+
+const ObjAnalytical = Callback(getObjAna1d)
+
+# args = (ref_goal,)
+function getObjRef1d(booster::Booster,freqs::Array{Float64},args::Tuple{Array{ComplexF64}})
+    return sum(abs.((ref1d(pos2dist(booster.pos; thickness=booster.thickness),
+        freqs; eps=booster.epsilon,thickness=booster.thickness)-args[1])))
+end
+
+# args = (ref_goal,scale_func)
+function getObjRef1d(booster::Booster,freqs::Array{Float64},args::Tuple{Array{ComplexF64},Function})
+    scale = args[2]
+
+    return sum(scale.(abs.((ref1d(pos2dist(booster.pos; thickness=booster.thickness),
+        freqs; eps=booster.epsilon,thickness=booster.thickness)-args[1]))))
+end
+
+const ObjRefLin(ref0) = Callback(getObjRef1d,(ref0,))
+const ObjRefSquare = Callback(getObjRef1d,(ref0,x->x^2))
+const ObjRefExp = Callback(getObjRef1d,(ref0,exp))
---
+++ src/utilities.jl
@@ -0,0 +1,73 @@
+###     convenient convenience functions for convenience
+
+export init, boost1d, findpeak, genFreqs, pos2dist, dist2pos, pNorm,
+        copy, getBoost1d
+
+init = [1.00334, 6.94754, 7.1766, 7.22788, 7.19717,
+        7.23776, 7.07746, 7.57173, 7.08019, 7.24657,
+        7.21708, 7.18317, 7.13025, 7.2198, 7.45585,
+        7.39873, 7.15403, 7.14252, 6.83105, 7.42282]*1e-3
+
+boost1d(spacs::Vector{Float64},f::Vector{Float64};eps::Real=24.,thickness::Real=1e-3) = 
+    abs2.(disk_system(f;
+        spacings=[spacs;0],disk_thickness=thickness,disk_epsilon=eps,
+        num_disk=length(spacs))[2])
+
+ref1d(spacs::Vector{Float64},f::Vector{Float64};eps::Real=24.,thickness::Real=1e-3) = 
+    disk_system(f; spacings=[spacs;0],disk_thickness=thickness,disk_epsilon=eps,
+        num_disk=length(spacs))[1]
+
+function findpeak(f0,n; eps=24.,thickness=1e-3,gran=1000,dev=0.1)
+    λ = 299792458.0/f0
+    B = zeros(gran)
+    D = range(1-dev; stop=1+dev,length=gran)*λ/2
+    for i in eachindex(D)
+        B[i] = boost1d(ones(n)*D[i],[f0]; eps=eps,thickness=thickness)[1]
+    end
+    return D[findmax(B)[2]]
+end
+
+function genFreqs(fcenter,fwidth; length=100)
+    return Array(range(fcenter-fwidth/2; stop=fcenter+fwidth/2,length=length))
+end
+
+function genFreqs(bounds; length=100)
+    return Array(range(bounds[1]; stop=bounds[2],length=length))
+end
+
+function pos2dist(pos::Array{Float64}; thickness=1e-3)
+    pos = [0; pos]
+    d = (pos[2:end]-pos[1:end-1])
+    d[2:end] .-= thickness
+    return d
+end
+
+function dist2pos(dist::Array{Float64}; thickness=1e-3)
+    return [sum(dist[1:i])+(i-1)*thickness for i in 1:length(dist)]
+end
+
+function pNorm(x; p=2)
+    return sum(@. abs(x)^p)^(1/p)
+end
+
+
+Base.copy(x::T) where T = T([getfield(x, k) for k ∈ fieldnames(T)]...)
+
+function getBoost1d(booster::Booster,freqs::Array{Float64})
+    return boost1d(pos2dist(booster.pos; thickness=booster.thickness),freqs;
+        eps=booster.epsilon,thickness=booster.thickness)
+end
+
+function getRef1d(booster::Booster,freqs::Array{Float64})
+    return ref1d(pos2dist(booster.pos; thickness=booster.thickness),freqs;
+        eps=booster.epsilon,thickness=booster.thickness)
+end
+
+
+
+
+function shiftdown!(x::Vector)
+    @inbounds for i in length(x)-1:-1:1
+        x[i+1] = x[i]
+    end
+end
