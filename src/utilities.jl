41267a04e530934d49f3992bc6b1f84cc6a6c7f0
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-09-14 15:14:28 +0200

    boost1d findpeak tand fix

--- src/utilities.jl
+++ src/utilities.jl
@@ -8,21 +8,26 @@ init = [1.00334, 6.94754, 7.1766, 7.22788, 7.19717,
         7.21708, 7.18317, 7.13025, 7.2198, 7.45585,
         7.39873, 7.15403, 7.14252, 6.83105, 7.42282]*1e-3
 
-boost1d(spacs::Vector{Float64},f::Vector{Float64};eps::Real=24.,thickness::Real=1e-3) = 
+boost1d(spacs::Vector{Float64},f::Vector{Float64}; 
+        eps::Real=24.,tand=0.,thickness::Real=1e-3) = 
     abs2.(disk_system(f;
         spacings=[spacs;0],disk_thickness=thickness,disk_epsilon=eps,
-        num_disk=length(spacs))[2])
+        tand=tand,num_disk=length(spacs))[2])
 
-ref1d(spacs::Vector{Float64},f::Vector{Float64};eps::Real=24.,thickness::Real=1e-3) = 
-    disk_system(f; spacings=[spacs;0],disk_thickness=thickness,disk_epsilon=eps,num_disk=length(spacs))[1]
+ref1d(spacs::Vector{Float64},f::Vector{Float64}; 
+        eps::Real=24.,tand=0.,thickness::Real=1e-3) = 
 
-function findpeak(f0,n; eps=24.,thickness=1e-3,gran=1000,dev=0.1)
+    disk_system(f; spacings=[spacs;0],disk_thickness=thickness,disk_epsilon=eps,
+        tand=tand,num_disk=length(spacs))[1]
+
+function findpeak(f0,n; eps=24.,tand=0.,thickness=1e-3,gran=1000,dev=0.1)
     λ = 299792458.0/f0
     B = zeros(gran)
     D = range(1-dev; stop=1+dev,length=gran)*λ/2
 
     for i in eachindex(D)
-        B[i] = boost1d(ones(n)*D[i],[f0]; eps=eps,thickness=thickness)[1]
+        B[i] = boost1d(ones(n)*D[i],[f0];
+            eps=eps,tand=tand,thickness=thickness)[1]
     end
 
     return D[findmax(B)[2]]
