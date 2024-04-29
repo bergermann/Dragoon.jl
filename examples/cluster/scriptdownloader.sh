33fce070b4fa2d66aaadef89d283273b69a0cd4f
bergermann <dominik.bergermann@gmx.de>
2024-04-29 14:49:52 +0200

    fix

--- examples/cluster/scriptdownloader.sh
+++ examples/cluster/scriptdownloader.sh
@@ -1,15 +1,13 @@
 
-DIR = "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/"
-
 ### get optimizers and stuff
-wget "${DIR}standard_settings.jl" -O "optimizers/standard_settings.jl"
-wget "${DIR}variants.txt" -O "optimizers/variants.txt"
+wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/standard_settings.jl" -O "optimizers/standard_settings.jl"
+wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/variants.txt" -O "optimizers/variants.txt"
 
-wget "${DIR}optimizers/nm1.jl" -O "optimizers/nm1.jl"
+wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/optimizers/nm1.jl" -O "optimizers/nm1.jl"
 
 ### get cluster scheduler scripts
-wget "${DIR}optimization.sh" -O "optimization.sh"
-wget "${DIR}scheduler.sh" -O "scheduler.sh"
+wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/optimization.sh" -O "optimization.sh"
+wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/main/examples/cluster/scheduler.sh" -O "scheduler.sh"
 
 ### update Dragoon
 julia -e "using Pkg; Pkg.add(url=\"https://github.com/bergermann/Dragoon.jl.git\"); Pkg.update()"
