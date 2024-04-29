7aaf03983c4c4bb851028b7dbb5575b0d639f530
bergermann <dominik.bergermann@gmx.de>
2024-04-29 10:13:45 +0200

    add cluster script loaders

--- examples/cluster/getdrgscripts.sh
+++ examples/cluster/getdrgscripts.sh
@@ -1,7 +1,7 @@
 
 
 ### download script loader
-wget "https://github.com/bergermann/Dragoon.jl/blob/main/examples/cluster/scriptloader.sh" -O "scriptloader.sh"
+wget "https://github.com/bergermann/Dragoon.jl/blob/main/examples/cluster/scriptloader.sh" -O "scriptdownloader.sh"
 
 ### run script loader
 sh scriptloader.sh
