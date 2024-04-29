d7d960be347a2864f638cbdaad3c35b0f3bd405c
bergermann <dominik.bergermann@gmx.de>
2024-04-29 10:43:34 +0200

    fix

--- examples/cluster/getdrgscripts.sh
+++ examples/cluster/getdrgscripts.sh
@@ -4,4 +4,4 @@
 wget "https://raw.githubusercontent.com/bergermann/Dragoon.jl/blob/main/examples/cluster/scriptdownloader.sh" -O "scriptdownloader.sh"
 
 ### run script loader
-sh scriptloader.sh
+sh scriptdownloader.sh
