fb1b60260672dec5e52a8302377191d4914044ea
bergermann <dominik.bergermann@gmx.de>
2024-05-27 15:21:12 +0200

    stuff

--- examples/analysis/analysis.jl
+++ examples/analysis/analysis.jl
@@ -2,14 +2,24 @@ using JLD2, Plots, ColorSchemes, Clustering, BoostFractor, Dragoon
 
 include("tools.jl")
 
-path = getPath(1e-6,100_000,22.025e9,50e6,10,20,24.0,0.0);
+# path = getPath(1e-5,1000,22.025e9,50e6,50,20,24.0,0.0);
+path = joinpath("examples","analysis","optimization data","2024_05_21-13_58_21.jld2")
 
-data = prepareDataAll1d(path,-12_000);
+# data = prepareDataAll1d(path);
+data = prepareData1d(path,0)
 
-# data = prepareData1d(path,-12_000);
+# data = prepareDataAll1d(path,-12_000);
 
-showQuality(data,-14000)
+showQuality(data,0)
 
 showDist(data,1000)
-c = showClusters(data,50,100);
+c = showClusters(data,10,200);
 
+freqsplot = genFreqs(22.025e9,150e6; n=1000)
+
+plot(data.freqs/1e9,)
+
+b = best(data)
+
+plot(data.freqs/1e9,b.boost)
+scatter(b.dist)
