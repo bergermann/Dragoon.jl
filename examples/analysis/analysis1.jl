4b03a5e0280d045c6b92e1a886c207e6b1ebc7a9
bergermann <dominik.bergermann@gmx.de>
2024-08-12 12:35:43 +0200

    more analysis tools

--- examples/analysis/analysis1.jl
+++ examples/analysis/analysis1.jl
@@ -9,7 +9,7 @@ include("tools.jl");
 # path = getPath("rand",100_000,22.025e9,50e6,10,20,24.0,0.0,"NM2","2024_05_29-15_05_16");
 
 # data = prepareDataAll1d(path,-10_000);
-data = prepareDataAll1d(getPath(),-14_500);
+data = prepareDataAll1d(getPath(),-14_000);
 # data = prepareData1d(path,-14_000);
 
 
@@ -22,15 +22,17 @@ freqsplot = genFreqs(22.025e9,150e6; n=1000);
 
 sortData!(data)
 b = best(data);
+histogram(-data.obj; xlabel="Minimum Boostfactor β²",title="Distribution of Converged States (20 Discs, ε=24)",
+    label=false,xlims=(floor(minimum(-data.obj)),ceil(maximum(-data.obj))))
 # showFields(data,data.freqs,freqsplot)
 # showDist(data[2])
 
 # showQuality(data)
 
-# showDist(data,1000; xlabel="disc index", ylabel="d_i [mm]")
+showDist(data,1000; xlabel="Disc Index", ylabel="d_i [mm]")
 # hline!([initdist]*1e3)
 
-# plot(data.freqs/1e9,data.boost; xlabel="frequency [GHz]",ylabel="boost factor β^2")
+# plot(data.freqs/1e9,data.boost; xlabel="Frequency [GHz]",ylabel="Boostfactor β²")
 
 c = showClusters(data,50,200);
 bc = [best(data[findall(isequal(i),c.assignments)]) for i in axes(c.centers,2)]
@@ -42,25 +44,30 @@ datanm = data[findall(isequal(:nm),data.tags)]; sortData!(datanm)
 datasa = data[findall(isequal(:sa),data.tags)]; sortData!(datasa)
 datals = data[findall(isequal(:ls),data.tags)]; sortData!(datals)
 
+showDist(datanm)
 showDistribution(datanm,d0)
+showDist(datasa)
 showDistribution(datasa,d0)
+showDist(datals)
 showDistribution(datals,d0)
 
 
 
-out = findOutliers(data,4.5e-3; showdistribution=true); length(out)
+out = findOutliers(data,6e-3; showdistribution=true); length(out)
 showDist(out)
 showFields(out,data.freqs,freqsplot)
 
+showFields(data[2],data.freqs,freqsplot)
 
-
-wiggle(data[1].pos[:,1],1e-6,10000,freqsplot,(22e9,22.05e9))
-wiggle(data[2].pos[:,1],1e-6,10000,freqsplot,(22e9,22.05e9))
+freqsplot1 = genFreqs(22.025e9,150e6; n=201);
+wiggle(data[1].pos[:,1],1e-6,10000,freqsplot,(22e9,22.05e9));
+wiggle(data[2].pos[:,1],1e-6,10000,freqsplot,(22e9,22.05e9));
 
 wigglewiggle(data[1].pos[:,1],collect(1:10)*1e-6,10000,freqsplot,(22e9,22.05e9))
 wigglewiggle(data[2].pos[:,1],collect(1:10)*1e-6,10000,freqsplot,(22e9,22.05e9))
 
+wigglewiggle(data[2].pos[:,1],collect(1:10)*1e-6,100,freqsplot,(22e9,22.05e9))
 
-wiggle(data[1].pos[:,1],5e-6,10000,freqsplot,(22e9,22.05e9))
-wiggle(data[2].pos[:,1],5e-6,10000,freqsplot,(22e9,22.05e9))
+wiggle(data[1].pos[:,1],5e-6,10000,freqsplot,(22e9,22.05e9));
+wiggle(data[2].pos[:,1],5e-6,10000,freqsplot,(22e9,22.05e9));
 
