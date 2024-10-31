f1607e486b44b97d53d6f35365f75551c5b0f55a
bergermann <dominik.bergermann@gmx.de>
2024-10-31 16:20:00 +0100

    transfer matrix fix

--- src/tm.jl
+++ src/tm.jl
@@ -1,82 +1,73 @@
 
 
-
+include("Analytical1D.jl")
 using LinearAlgebra: mul!, det
 using StaticArrays
+using BenchmarkTools
+using Plots
 
 
 const c0 = 299792458.
 
+d = [1.00334, 6.94754, 7.1766, 7.22788, 7.19717,
+    7.23776, 7.07746, 7.57173, 7.08019, 7.24657,
+    7.21708, 7.18317, 7.13025, 7.2198, 7.45585,
+    7.39873, 7.15403, 7.14252, 6.83105, 7.42282]*1e-3
+d_ = [d; 0]
+
+freqs = collect(range(21.95,22.1,100))*1e9
+
+ref0 = disk_system(freqs; num_disk=20,tand=0.,disk_epsilon=24.0,disk_thickness=1e-3,spacings=d_)[1]
+@btime disk_system($freqs; num_disk=20,tand=0.,disk_epsilon=24.0,disk_thickness=1e-3,spacings=$d_);
+
 
 function tm(freqs,distances; tand=0.0,eps=24.0,disc_thickness=1e-3,nm=1e15)
     ϵ  = eps*(1.0-1.0im*tand)
-    nd = sqrt(ϵ)
-    A  = 1/ϵ-1
-    A0 = complex(1-1/sqrt(nm))
+    nd = sqrt(ϵ); nm = complex(nm)
+    ϵm = nm^2
+    A  = 1-1/ϵ
+    A0 = 1-1/ϵm
 
-    # RB  = Matrix{ComplexF64}(undef,2,length(freqs))
-
-    # Gd = ComplexF64[1+nd 1-nd; 1-nd 1+nd]; Gd ./= 2
-    # Gv = ComplexF64[nd+1 nd-1; nd-1 nd+1]; Gv ./= 2nd
-    # G0 = ComplexF64[1+nm 1-nm; 1-nm 1+nm]; G0 ./= 2
-    # T  = ComplexF64[1+nd 1-nd; 1-nd 1+nd]; T  ./= 2
-
-    # S  = ComplexF64[ A 0.0im; 0.0im  A]; S  ./= 2
-    # S0 = ComplexF64[A0 0.0im; 0.0im A0]; S0 ./= 2
-    # M  = copy(S)
-
-    # W  = Matrix{ComplexF64}(undef,2,2)
-
-    l1 = length(freqs); l2 = 2*l1
+    l1 = length(freqs)
     RB  = MMatrix{2,l1,ComplexF64}(undef)
 
-    Gd = SMatrix{2,2,ComplexF64}((1+nd)/2, (1-nd)/2, (1-nd)/2, (1+nd)/2) # ; Gd ./= 2
-    Gv = SMatrix{2,2,ComplexF64}((nd+1)/2nd, (nd-1)/2nd, (nd-1)/2nd, (nd+1)/2nd) # ; Gv ./= 2nd
-    G0 = SMatrix{2,2,ComplexF64}((1+nm)/2, (1-nm)/2, (1-nm)/2, (1+nm)/2) # ; G0 ./= 2
+    Gd = SMatrix{2,2,ComplexF64}((1+nd)/2,   (1-nd)/2,   (1-nd)/2,   (1+nd)/2)
+    Gv = SMatrix{2,2,ComplexF64}((nd+1)/2nd, (nd-1)/2nd, (nd-1)/2nd, (nd+1)/2nd)
+    G0 = SMatrix{2,2,ComplexF64}((1+nm)/2,   (1-nm)/2,   (1-nm)/2,   (1+nm)/2)
     T  = MMatrix{2,2,ComplexF64}(undef); T .= Gd
 
-    S  = SMatrix{2,2,ComplexF64}( A/2, 0.0im, 0.0im,  A/2) # ; S  ./= 2
-    S0 = SMatrix{2,2,ComplexF64}(A0/2, 0.0im, 0.0im, A0/2) # ; S0 ./= 2
+    S  = SMatrix{2,2,ComplexF64}( A/2, 0.0im, 0.0im,  A/2)
+    S0 = SMatrix{2,2,ComplexF64}(A0/2, 0.0im, 0.0im, A0/2)
     M  = MMatrix{2,2,ComplexF64}(undef); M .= S
 
     W  = MMatrix{2,2,ComplexF64}(undef) # work matrix for multiplication
 
-    d = 0
-
-    @views for j in eachindex(freqs)
-        pd1 = cispi(+2*freqs[j]*nd*disc_thickness/c0)
-        pd2 = cispi(-2*freqs[j]*nd*disc_thickness/c0)
+    for j in eachindex(freqs)
+        pd1 = cispi(-2*freqs[j]*nd*disc_thickness/c0)
+        pd2 = cispi(+2*freqs[j]*nd*disc_thickness/c0)
 
         for i in Iterators.reverse(eachindex(distances))
             T[:,1] .*= pd1
-            T[:,2] .*= pd2
+            T[:,2] .*= pd2 # T = Gd*Pd
 
-            d += disc_thickness
+            mul!(W,T,S); M .-= W    # M = Gd*Pd*S_-1
 
-            display(d)
+            mul!(W,T,Gv); T .= W    # T *= Gd*Pd*Gv
 
-            mul!(W,T,Gv); T .= W
-
-            mul!(W,T,S); M .+= W
-        
-            T[:,1] .*= cispi(+2*freqs[j]*distances[i]/c0)
-            T[:,2] .*= cispi(-2*freqs[j]*distances[i]/c0)
-
-            d += distances[i]
-            
-            display(d)
+            T[:,1] .*= cispi(-2*freqs[j]*distances[i]/c0)
+            T[:,2] .*= cispi(+2*freqs[j]*distances[i]/c0)   # T = Gd*Pd*Gv*Gd*S_-1
 
             if i > 1
+                mul!(W,T,S); M .+= W
                 mul!(W,T,Gd); T .= W
-                mul!(W,T,S); M .-= W
             else
-                mul!(W,T,G0); T .= W
                 mul!(W,T,S0); M .+= W
+                mul!(W,T,G0); T .= W
             end
         end
         
         RB[1,j] = T[1,2]/T[2,2]
-        RB[2,j] = M[1,1]+M[1,2]
+        RB[2,j] = M[1,1]+M[1,2]-(M[2,1]+M[2,2])*T[1,2]/T[2,2]
 
         # display(det(T))
 
@@ -87,20 +78,51 @@ function tm(freqs,distances; tand=0.0,eps=24.0,disc_thickness=1e-3,nm=1e15)
     return RB
 end
 
-rb = @btime tm($freqs,$d; nm=1e10)
-rb = tm(freqs[1],d; nm=1e9); ref = rb[1,:]; boost = rb[2,:];
+rb = @btime tm($freqs,$d; nm=1e10);
+rb = tm(freqs,d; nm=1e9); ref = rb[1,:]; boost = rb[2,:];
 
 plot(freqs/1e9,abs2.(boost))
-plot(freqs/1e9,abs.(ref-ref0))
+# plot(freqs/1e9,abs.(ref-ref0))
+
+r1,b1 = tm(22.025e9,[7e-3]; nm=1e9,eps=24.)[:]; abs2(b1)
+r2,b2 = disk_system([22.025e9]; num_disk=1,disk_epsilon=24.,disk_thickness=1e-3,spacings=[7e-3,0]); abs2(b2[])
 
 
-function test(f)
-    l1 = length(f)
 
-    MMatrix{2,l1,ComplexF64}(undef)
 
-    return
-end
 
-@btime test($freqs)
-test(freqs)
+
+
+
+nm = complex(1e20); nd = complex(sqrt(24.0))
+Av = 1; Ad = 1/24.; Am = 1/nm^2
+
+G0 = [1+nm 1-nm; 1-nm 1+nm]/2
+G1 = [nd+1 nd-1; nd-1 nd+1]/2nd
+G2 = [1+nd 1-nd; 1-nd 1+nd]/2
+
+pd1 = cispi(-2*22.025e9*nd*1e-3/c0)
+pd2 = cispi(+2*22.025e9*nd*1e-3/c0)
+pv1 = cispi(-2*22.025e9*1*7e-3/c0)
+pv2 = cispi(+2*22.025e9*1*7e-3/c0)
+P0 = [  1 0; 0   1]
+P1 = [pv1 0; 0 pv2]
+P2 = [pd1 0; 0 pd2]
+
+T = G2*P2*G1*P1*G0*P0; r = T[1,2]/T[2,2]
+
+
+
+S0 = [1 0; 0 1]*(Av-Am)/2
+S1 = [1 0; 0 1]*(Ad-Av)/2
+S2 = [1 0; 0 1]*(Av-Ad)/2
+
+M = ComplexF64[0 0; 0 0]
+M += S2
+M += G2*P2*S1
+M += G2*P2*G1*P1*S0
+
+abs2((M[1,1]+M[1,2]-(M[2,1]+M[2,2])*T[1,2]/T[2,2]))
+
+
+
