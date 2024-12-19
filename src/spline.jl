58d1dfa82036b60193fa35430ff9ab7702c9d36b
bergermann <dominik.bergermann@gmx.de>
2024-12-19 17:48:19 +0100

    weeeeeeeee

--- src/spline.jl
+++ src/spline.jl
@@ -30,35 +30,32 @@ function cSpline(x,y; b=[1,0,0,1],c=[0,0])
     @assert length(x) > 1 "Need at least 2 knots."
 
     h = x[2]-x[1]
-    n = length(x)-1
+    n = length(x)
 
-    A = zeros(n,4); A[:,1] .= y[1:end-1]
+    A = zeros(n-1,4)
 
-    du = ones(Float64,n-1); du[1] = b[2]
-    d  = ones(Float64,n)*4; d[1]  = b[1]; d[end] = b[4]
+    du = ones(Float64,n-1);   du[1] = b[2]
+    d  = ones(Float64,n)*4;    d[1] = b[1]; d[end] = b[4]
     dl = ones(Float64,n-1); dl[end] = b[3]
 
     A2 = Tridiagonal(dl, d, du); A2_ = inv(A2)
 
     D = zeros(Float64,n);
-    D[1] = -c[1]-y[1];     D[2] = y[1]
-    D[end] = -c[2]-y[end]; D[end-1] = y[end]
-
+    D[1] = c[1]; D[end] = c[2]
     for i in 2:n-1
-        D[i-1] += y[i]
-        D[i] -= 2y[i]
-        D[i+1] += y[i]
+        D[i] = y[i+1]-2y[i]+y[i-1]
     end
+    D .*= 3/h; a2 = A2_*D
 
-    D .*= 3/h
-
-    A[:,3] = A2_*D
-    @. A[1:end-1,4] = (A[2:end,3]-A[1:end-1,3])/3h; A[end,4] = -A[end,3]/3h
-    @. A[:,2] = (y[2:end]-y[1:end-1])/h + A[:,3]*h + A[:,4]*h^2
+    @. A[:,4] = (a2[2:end]-a2[1:end-1])/3h
+    @. A[:,3] = a2[1:end-1]
+    @. A[:,2] = (y[2:end]-y[1:end-1])/h - A[:,3]*h - A[:,4]*h^2
+    @. A[:,1] =  y[1:end-1]
 
     display(A2)
     display(D)
     display(A)
+    display(A[:,4]-((y[2:end]-y[1:end-1])/h^3-A[:,2]/h^2-A[:,3]/h))
 
     return Spline(3,x,A)
 end
@@ -95,19 +92,21 @@ function spline(spline::Spline,x::Real)
 end
 
 
-x = collect(0:6)
-# y = rand(length(x))
+x = collect(0:10)
+y = rand(length(x))
 # y = ones(length(x))
-y = collect(0:6)
+# y = collect(0:6)
+# y = collect(6:-1:0)
+# y = sin.(x*pi/4)
 
-S = cSpline(x,y; b=[1,0,0,1],c=[1,1])
+S = cSpline(x,y; b=[1,0,0,1],c=[0,0])
 
 0
 
 spline(S,4)
 y[5]
 
-x_ = collect(0:0.01:6)
+x_ = collect(x[1]:0.01:x[end])
 S_ = [spline(S,x_[i]) for i in eachindex(x_)]
 
 scatter(x,y)
